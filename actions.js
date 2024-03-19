1e5e3430edcf878d4473bdc831607679826ffda8
Elliot Matson <ematson5897@gmail.com>
2024-03-19 17:19:07 -0500

    Initial commit

--- actions.js
+++ actions.js
@@ -16,5 +16,147 @@ module.exports = function (self) {
 				console.log('Hello world!', event.options.num)
 			},
 		},
+		writeFloatPoint: {
+			name: 'Write Float Point',
+			description: 'Write a float point to InfluxDB',
+			options: [
+				{
+					id: 'measurement',
+					type: 'textinput',
+					label: 'Measurement',
+					default: 'example',
+				},
+				{
+					id: 'field',
+					type: 'textinput',
+					label: 'Field',
+					default: 'value',
+				},
+				{
+					id: 'value',
+					type: 'textinput',
+					label: 'Value',
+					default: '0',
+					useVariables: true,
+				},
+			],
+			callback: async (event) => {
+				if (self.writeApi) {
+					const point = new self.influx.Point(event.options.measurement).floatField(
+						event.options.field,
+						parseFloat(event.options.value)
+					)
+					self.writeApi.writePoint(point)
+				}
+			},
+		},
+		writeStringPoint: {
+			name: 'Write String Point',
+			description: 'Write a string point to InfluxDB',
+			options: [
+				{
+					id: 'measurement',
+					type: 'textinput',
+					label: 'Measurement',
+					default: 'example',
+				},
+				{
+					id: 'field',
+					type: 'textinput',
+					label: 'Field',
+					default: 'value',
+				},
+				{
+					id: 'value',
+					type: 'textinput',
+					label: 'Value',
+					default: '0',
+					useVariables: true,
+				},
+			],
+			callback: async (event) => {
+				if (self.writeApi) {
+					const point = new self.influx.Point(event.options.measurement).stringField(
+						event.options.field,
+						event.options.value
+					)
+					self.writeApi.writePoint(point)
+				}
+			},
+		},
+		writeBooleanPoint: {
+			name: 'Write Boolean Point',
+			description: 'Write a boolean point to InfluxDB',
+			options: [
+				{
+					id: 'measurement',
+					type: 'textinput',
+					label: 'Measurement',
+					default: 'example',
+				},
+				{
+					id: 'field',
+					type: 'textinput',
+					label: 'Field',
+					default: 'value',
+				},
+				{
+					id: 'value',
+					type: 'dropdown',
+					label: 'Value',
+					default: 'true',
+					choices: [
+						{ id: 'true', label: 'True' },
+						{ id: 'false', label: 'False' },
+					],
+				},
+			],
+			callback: async (event) => {
+				if (self.writeApi) {
+					const point = new self.influx.Point(event.options.measurement).booleanField(
+						event.options.field,
+						event.options.value === 'true'
+					)
+					self.writeApi.writePoint(point)
+				}
+			},
+		},
+		writeInfluxLine: {
+			name: 'Write Influx Line',
+			description: 'Writes a single line to InfluxDB. The line must be in the InfluxDB line protocol format.',
+			options: [
+				{
+					id: 'measurement',
+					type: 'textinput',
+					label: 'Measurement',
+					default: 'exampleMeasurement',
+					useVariables: true,
+				},
+				{
+					id: 'tags',
+					type: 'textinput',
+					label: 'Tags',
+					default: 'tag1=value1,tag2=value2',
+					useVariables: true,
+				},
+				{
+					id: 'fields',
+					type: 'textinput',
+					label: 'Fields',
+					default: 'field1=value1,field2=value2',
+					useVariables: true,
+				},
+			],
+			callback: async (event) => {
+				if (self.writeApi) {
+					self.writeApi.writeRecord(
+						`${event.options.measurement},${event.options.tags} ${event.options.fields} ${Date.now() * 1000000}`
+					)
+					self.writeApi.flush()
+					// log the line to the console
+					self.log('debug', `Sent to Influx: ${event.options.measurement},${event.options.tags} ${event.options.fields} ${Date.now() * 1000000}`)
+				}
+			},
+		},
 	})
 }
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const UpgradeScripts = require('./upgrades')
 const UpdateActions = require('./actions')
 const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
+const { InfluxDB } = require('@influxdata/influxdb-client')
+const { PingAPI, HealthAPI } = require('@influxdata/influxdb-client-apis')
 
 class ModuleInstance extends InstanceBase {
 	constructor(internal) {
@@ -11,8 +13,39 @@ class ModuleInstance extends InstanceBase {
 
 	async init(config) {
 		this.config = config
+		this.influx = new InfluxDB({
+			url: this.config.url,
+			token: this.config.token,
+			timeout: 5000,
+		})
+		this.ping = new PingAPI(this.influx)
+		
+		this.ping
+			.getPing() // check ping
+			.then(() => {
+				this.log('info', 'InfluxDB is pingable')
+				this.updateStatus(InstanceStatus.Ok)
+			})
+			.catch((error) => {
+				this.log('error', 'InfluxDB is not pingable')
+				this.updateStatus(InstanceStatus.ConnectionFailure, 'InfluxDB is not pingable')
+			})
 
-		this.updateStatus(InstanceStatus.Ok)
+		this.health = new HealthAPI(this.influx)
+
+		this.health
+			.getHealth() // check health
+			.then(() => {
+				this.log('info', 'InfluxDB is healthy')
+				this.updateStatus(InstanceStatus.Ok)
+			})
+			.catch((error) => {
+				this.log('error', 'InfluxDB is not healthy')
+				this.updateStatus(InstanceStatus.ConnectionFailure, 'InfluxDB is not healthy')
+			})
+
+		this.writeApi = this.influx.getWriteApi(this.config.org, this.config.bucket)
+
 
 		this.updateActions() // export actions
 		this.updateFeedbacks() // export feedbacks
@@ -24,7 +57,7 @@ class ModuleInstance extends InstanceBase {
 	}
 
 	async configUpdated(config) {
-		this.config = config
+		this.init(config)
 	}
 
 	// Return config fields for web config
@@ -32,17 +65,32 @@ class ModuleInstance extends InstanceBase {
 		return [
 			{
 				type: 'textinput',
-				id: 'host',
-				label: 'Target IP',
+				id: 'url',
+				label: 'InfluxDB URL',
 				width: 8,
-				regex: Regex.IP,
+				default: 'http://localhost:8086',
+				regex: Regex.SOMETHING,
 			},
 			{
 				type: 'textinput',
-				id: 'port',
-				label: 'Target Port',
+				id: 'token',
+				label: 'InfluxDB Token',
+				width: 8,
+				regex: Regex.SOMETHING,
+			},
+			{
+				type: 'textinput',
+				id: 'org',
+				label: 'InfluxDB Organization',
 				width: 4,
-				regex: Regex.PORT,
+				regex: Regex.SOMETHING,
+			},
+			{
+				type: 'textinput',
+				id: 'bucket',
+				label: 'InfluxDB Bucket',
+				width: 4,
+				regex: Regex.SOMETHING,
 			},
 		]
 	}
